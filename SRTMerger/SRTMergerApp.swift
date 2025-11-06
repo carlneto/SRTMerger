61cdd2c2ef68d80b9687b3ee3b2820fbd57225e0
carlneto <carlneto@icloud.com>
2025-11-06 12:00:17 +0000

    Initial Commit

---
+++ SRTMerger/SRTMergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRTMergerApp.swift
+//  SRTMerger
+//
+//  Created by Carlos Neto on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SRTMergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
